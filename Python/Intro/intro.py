de34f770fc026835e132bcb1d7f29f2b4be269cb
W4H33D <85181215+W4H33D@users.noreply.github.com>
2026-05-09 16:27:11 +0500

    Update intro.py

--- Python/Intro/intro.py
+++ Python/Intro/intro.py
@@ -1,5 +1,5 @@
-# Assingment Operator 
-fixed_costs = 300 
+# Assingment Operator
+fixed_costs = 300
 variable_costs = 100
 revenue = 1000
 
@@ -24,7 +24,7 @@ units_to_buy = profit // 70
 # Modulo: How much cash is left over after buying those units?
 leftover_cash = profit % 70
 
-# Printing Every Values 
+# Printing Raw Values
 print(total_costs)  # Output: 400
 print(profit)  # Output: 600
 print(projected_revenue)  # Output: 2000
@@ -32,3 +32,25 @@ print(growth_factor)  # Output: 8
 print(margin)  # Output: 0.6
 print(units_to_buy)  # Output: 8
 print(leftover_cash)  # Output: 40
+
+# Handling Strings And It's Formatting
+banner = "dashbord section initialized...\n".title() # Method We can use: .title(), .upper(), .lower(), and .capitalize()
+dashboard = "------------------------\n" + \
+            "Total Cost Of Our Product: " + \
+            str(total_costs) + \
+            f"\nTotal Profit: {profit}\n" + \
+            f"Total Revenue: {projected_revenue} with the " + \
+            f"Growth Factor: {growth_factor}\n" + \
+            f"Our Margins: {margin}\n" + \
+            f"{units_to_buy = }\n" + \
+            f"{leftover_cash = }\n" + \
+            "------------------------\n"
+"""
+On line 40 we use Type Casting Method
+On line 41 onwards we Formated String Format. Notice the 'f' before every String.
+On line 45 and 46 We use f-string debugging introduced in Python 3.8
+"""
+# Print Formatted Output Using String formating
+print(f"{banner}{dashboard.capitalize()}")
+
+
